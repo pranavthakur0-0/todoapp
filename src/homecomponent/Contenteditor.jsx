a977bc255c0e34f04eab63353427ccc8266a7bd4
pranavthakur0-0 <pranavthakurdew@gmail.com>
2022-12-30 20:55:13 +0530

    console

--- src/homecomponent/Contenteditor.jsx
+++ src/homecomponent/Contenteditor.jsx
@@ -13,12 +13,10 @@ function formatDoc(cmd, value=null)
   if(value)
   {
     document.execCommand(cmd, false, value);
-    console.log(document.execCommand(cmd, false, value))
   }
   else
   {
     document.execCommand(cmd);
-    console.log(document.execCommand(cmd));
   }
 }
 
